cc820c770f962ef74c10b395f7d66f05415fdfa4
Marko Ciric <marko.ciric2002@gmail.com>
2025-07-27 13:06:49 +0200

    logout admin

--- src/app/pages/admin/admin.page.ts
+++ src/app/pages/admin/admin.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService, AppUser } from '../../services/user.service';
-
-import { addIcons } from 'ionicons';
-import { library, playCircle, radio, search } from 'ionicons/icons';
+import { NavController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -11,13 +10,9 @@ import { library, playCircle, radio, search } from 'ionicons/icons';
 })
 export class AdminPage implements OnInit {
   users: AppUser[] = [];
-  constructor(private userService: UserService) {
-    /**
-     * Any icons you want to use in your application
-     * can be registered in app.component.ts and then
-     * referenced by name anywhere in your application.
-     */
-    addIcons({ library, playCircle, radio, search });
+  constructor(private userService: UserService, private auth: AuthService,
+    private navCtrl: NavController) {
+
   }
 
   ngOnInit() {
@@ -26,5 +21,10 @@ export class AdminPage implements OnInit {
       error: (err) => console.error('Greška pri učitavanju korisnika', err),
     });
   }
-
+logout() {
+    // Obriši credentiale iz lokalnog skladišta
+    this.auth.logout();             // ili localStorage.clear() u zavisnosti od tvog servisa
+    // Vratimo se na login kao koren aplikacije
+    this.navCtrl.navigateRoot('/login');
+  }
 }
